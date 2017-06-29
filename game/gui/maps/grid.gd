39dd8442f899015a67a286f285d036a25f24908b
Friederich Hass <angelus.tenebrae@gmail.com>
2017-06-29 21:32:06 +0200

    refactor discoverability grid code

---
+++ game/gui/maps/grid.gd
@@ -0,0 +1,97 @@
+extends Sprite
+
+# Tracks map tiles player has already been in
+
+# This information is stored in a 2D boolean array and rendered in an image texture
+# Unfortunately, other algorithms that involve displaying the discovered tiles as a set of
+# rectangles is NP Complete if you need there to be as few rectangles as possible and you allow
+# for the possibility of holes in between the rectangles, so we put pixels in an image texture
+# instead.
+
+# It's unfortunately hard to avoid O(n^2) or worse here though...
+
+const GRID_SIZE = Vector2(800 / 2, 592 / 2)
+const GRIDFORMAT = Image.FORMAT_RGBA
+const GRIDCOLOR = Color(79.69/255, 0, 1, 0.5)
+
+var texture
+var image
+var HudMap = preload("res://gui/maps/HudMap.gd")
+var grid
+
+func _ready():
+	pass
+
+func init(pos, width, height):
+	set_offset(pos)
+	texture = ImageTexture.new()
+	texture.create(width, height, GRIDFORMAT)
+	image = Image(texture.get_width(), texture.get_height(), false, GRIDFORMAT)
+	texture.set_data(image)
+	set_texture(texture)
+	grid = Array()
+	var gridy = ceil(float(height) / (HudMap.MAP_SCALE * GRID_SIZE.y))
+	var gridx = ceil(float(width) / (HudMap.MAP_SCALE * GRID_SIZE.x))
+	for i in range(0, gridy):
+		var row = Array()
+		for j in range(0, gridx):
+			row.append(false)
+		grid.append(row)
+
+# set a specified range of tiles as discovered
+# range consists of indexes in the grid
+func mark_grid(indexx, indexy):
+	var new_tiles = false
+	#print("check grid")
+	#print(indexx)
+	#print(indexy)
+	for i in range(indexy.x, indexy.y + 1):
+		var row = grid[i]
+		var row_size = row.size()
+		for j in range(indexx.x, indexx.y + 1):
+			var cell = row[j]
+			if (!cell):
+				grid[i][j] = true
+				new_tiles = true
+				draw_grid(j, i)
+				#print("new tile discovered")
+	if (new_tiles):
+		texture.set_data(image)
+		#print(grid)
+
+# Render all discovered tiles at once
+func render_grid():
+	print(grid)
+	
+	var col_size = grid.size()
+	for y in range(0, col_size):
+		var row = grid[y]
+		var row_size = row.size()
+		for x in range(0, row_size):
+			var cell = row[x]
+			if (cell):
+				draw_grid(x, y)
+	texture.set_data(image)
+
+# Draw discovered tile at the specified position
+# Position is given in grid indexes.
+func draw_grid(x, y):
+	var startx = max(round(GRID_SIZE.x * HudMap.MAP_SCALE * x), 0)
+	var starty = max(round(GRID_SIZE.y * HudMap.MAP_SCALE * y), 0)
+	var endx = round(min(GRID_SIZE.x * HudMap.MAP_SCALE * (x + 1), image.get_width())) + 1
+	var endy = round(min(GRID_SIZE.y * HudMap.MAP_SCALE * (y + 1), image.get_height())) + 1
+	#print("draw grid")
+	#print(x)
+	#print(y)
+	#print(grid_image.get_width())
+	#print(grid_image.get_height())
+	#print(boundaries[0])
+	#print(rooms[current_map].get_node("grid").get_offset())
+	#print(grid_range)
+	#print(startx)
+	#print(endx)
+	#print(starty)
+	#print(endy)
+	for i in range(starty, endy):
+		for j in range(startx, endx):
+			image.put_pixel(j, i, GRIDCOLOR)
