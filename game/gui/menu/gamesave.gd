a1f4b8865e70e7565ee6cb49e9fdb8368b65e0df
Friederich Hass <angelus.tenebrae@gmail.com>
2017-07-02 16:57:01 +0200

    display game completion in load/save menu

--- game/gui/menu/gamesave.gd
+++ game/gui/menu/gamesave.gd
@@ -1,5 +1,32 @@
 extends "res://gui/dialogue/choice.gd"
 
+# completion totals
+const DISCOVERY_FRIEDERICH = [
+"LVL_START",
+"LVL_DUNGEON",
+"LVL_BERGFORTRESS",
+"LVL_MARBLECASTLE",
+"LVL_AQUADUCT2",
+"LVL_LAVACAVE",
+"LVL_WINTERISLANDCASTLE"
+]
+const DISCOVERY_ADELA = [
+"LVL_START",
+"LVL_ICECAVE",
+"LVL_AQUADUCT",
+"LVL_CAPECRYPT",
+"LVL_MAUSOLEUM",
+"LVL_SPRINGISLANDCASTLE"
+]
+# TODO - include tiles from catacombs as well
+const QUEST_TOTAL = 16
+const MAGIC_TOTAL = 7
+const SCROLL_TOTAL = 4
+var locationfactory = preload("res://levels/LocationFactory.gd").new()
+var itemfactory = preload("res://scenes/items/ItemFactory.gd").new()
+var friederich_discovery_total = 0
+var adela_discovery_total = 0
+
 var characterBG
 var id
 var idnr
@@ -31,6 +58,14 @@ signal echo
 signal loadgame
 
 func _ready():
+	var size = DISCOVERY_FRIEDERICH.size()
+	friederich_discovery_total = 0
+	for i in range(0, size):
+		friederich_discovery_total += locationfactory.locations[DISCOVERY_FRIEDERICH[i]].tiles
+	size = DISCOVERY_ADELA.size()
+	adela_discovery_total = 0
+	for i in range(0, size):
+		adela_discovery_total += locationfactory.locations[DISCOVERY_ADELA[i]].tiles
 	serialization = Globals.get("serialization")
 	optionsGroup = get_node("options")
 	if (loadonly):
@@ -227,14 +262,42 @@ func save_from_data(data):
 
 func displayGameData(data):
 	gameData = data
+	var discovery_total = 0
+	var completion = 0
 	if (data.player.character == "adela"):
 		characterBG.set_texture(preload("res://gui/save/bgs/adela.png"))
+		discovery_total = adela_discovery_total
 	else:
 		characterBG.set_texture(preload("res://gui/save/bgs/friederich.png"))
+		discovery_total = friederich_discovery_total
+	var discovery = 0
+	var quests = 0
+	for level in data.levels.data:
+		var level_data = data.levels.data[level]
+		discovery += level_data.tiles
+		if (level_data.complete):
+			quests += 1
+	completion += 0.2 * discovery / discovery_total + 0.2 * quests / QUEST_TOTAL
+	var size = data.inventory.magic.size()
+	var magic = 0
+	for i in range(0, size):
+		magic += 1
+	completion += 0.2 * magic / MAGIC_TOTAL
+	completion += 0.2 * data.inventory.scrolls.keys().size() / SCROLL_TOTAL
+	var item_type = 0
+	var item_type_total = 0
+	for item in itemfactory.items:
+		if (itemfactory.items[item].type == "item"):
+			item_type_total += 1
+	for item in data.inventory.items:
+		if (itemfactory.items[item].type == "item"):
+			item_type += 1
+	completion += 0.2 * item_type / item_type_total
+	completion = round(completion * 10000) / 100
 	saveGroup.get_node("location").set_text(data.location)
 	saveGroup.get_node("gold").set_text(str(data.inventory.gold) + "G")
 	saveGroup.get_node("stats").set_text("LV" + str(data.player.stats.level) + " " + str(data.player.stats.currentHp) + "/" + str(data.player.stats.hp) + "HP " + str(data.player.stats.currentMp) + "/" + str(data.player.stats.mp) + "MP")
-	saveGroup.get_node("previewstats").set_text(str(data.deaths) + "    100%    " + str(data.date[0]).pad_zeros(2) + "." + str(data.date[1]).pad_zeros(2) + "." + str(data.date[2]))
+	saveGroup.get_node("previewstats").set_text(str(data.deaths) + "    " + str(completion) + "%    " + str(data.date[0]).pad_zeros(2) + "." + str(data.date[1]).pad_zeros(2) + "." + str(data.date[2]))
 	saveGroup.get_node("playtime").set_text(gameclock_class.display_time(int(data.playtime)))
 
 func _on_choice_focus_enter():
