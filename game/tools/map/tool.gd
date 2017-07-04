425fcb7cf1b2a7c10f4cdf558323646acb7ce32b
Friederich Hass <angelus.tenebrae@gmail.com>
2017-07-04 11:00:55 +0200

    Allow map tool to count special tiles

--- game/tools/map/tool.gd
+++ game/tools/map/tool.gd
@@ -56,51 +56,73 @@ var roomobj = preload("res://tools/map/room.tscn")
 var levelobj = preload("res://tools/map/level.tscn")
 var container
 
+var gridwidth = round(Grid.GRID_SIZE.x * HudMap.MAP_SCALE)
+var gridheight = round(Grid.GRID_SIZE.y * HudMap.MAP_SCALE)
+var normal_size = gridwidth * gridheight
+
 func _ready():
 	container = get_node("statistics/container")
-	var gridwidth = round(Grid.GRID_SIZE.x * HudMap.MAP_SCALE)
-	var gridheight = round(Grid.GRID_SIZE.y * HudMap.MAP_SCALE)
-	var normal_size = gridwidth * gridheight
+
+	# special rooms
+	var catacombsdir = "res://levels/common"
+	var catacombs = add_level(catacombsdir)
+	var catacombs_total = analyze_room(catacombsdir, "catacombs.tscn")
+	catacombs.get_node("total/value").set_text(str(catacombs_total))
+
+	var colosseumdir = "res://levels/sandbox"
+	var colosseum = add_level(colosseumdir)
+	var colosseum_total = analyze_room(colosseumdir, "colosseum-prototype0-0.tscn")
+	colosseum_total += analyze_room(colosseumdir, "colosseum-prototype0-1.tscn")
+	colosseum.get_node("total/value").set_text(str(colosseum_total))
+
+	# normal rooms
 	var size = list.size()
 	for i in range(0, size):
 		var leveldir = list[i]
-		var level = levelobj.instance()
-		level.get_node("title/value").set_text(leveldir)
 		var dir = Directory.new()
 		var file = File.new()
+		var level = add_level(leveldir)
 		var level_total = 0
-		container.add_child(level)
 		if (dir.dir_exists(leveldir)):
 			if(dir.open(leveldir) == 0):
 				dir.list_dir_begin()
 				var filename = dir.get_next()
 				while (filename != ""):
 					if (!dir.current_is_dir()):
-						var room = load(leveldir + "/" + filename).instance()
-						var boundaries = room.get_node("tilemap/boundaries")
-						var nw = boundaries.get_node("NW").get_global_pos()
-						var se = boundaries.get_node("SE").get_global_pos()
-						var imagewidth = se.x - nw.x
-						var imageheight = se.y - nw.y
-						var gridy = ceil(float(imageheight) / Grid.GRID_SIZE.y)
-						var gridx = ceil(float(imagewidth) / Grid.GRID_SIZE.x)
-						var vertical_size = floor(imagewidth * HudMap.MAP_SCALE) - (gridwidth * (gridx - 1))
-						var horizontal_size = floor(imageheight * HudMap.MAP_SCALE) - (gridheight * (gridy - 1))
-						var vertical_area = vertical_size * gridheight
-						var horizontal_area = horizontal_size * gridwidth
-						var corner_area = horizontal_size * vertical_size
-						var normal_area = (gridx - 1) * gridwidth * (gridy - 1) * gridheight
-						var total = normal_area + vertical_area * (gridy - 1) + horizontal_area * (gridx - 1) + corner_area
-						level_total += total
-						var room_node = roomobj.instance()
-						room_node.get_node("title/value").set_text(filename)
-						room_node.get_node("grid/value").set_text(str(gridx) + "x" + str(gridy))
-						room_node.get_node("total/value").set_text(str(total))
-						room_node.get_node("size/value").set_text(str(normal_size))
-						room_node.get_node("horizontal/value").set_text(str(horizontal_area))
-						room_node.get_node("vertical/value").set_text(str(vertical_area))
-						room_node.get_node("corner/value").set_text(str(corner_area))
-						container.add_child(room_node)
+						level_total += analyze_room(leveldir, filename)
 					filename = dir.get_next()
 				dir.list_dir_end()
 		level.get_node("total/value").set_text(str(level_total))
+
+func add_level(leveldir):
+	var level = levelobj.instance()
+	level.get_node("title/value").set_text(leveldir)
+	container.add_child(level)
+	return level
+
+func analyze_room(leveldir, filename):
+	var room = load(leveldir + "/" + filename).instance()
+	var boundaries = room.get_node("tilemap/boundaries")
+	var nw = boundaries.get_node("NW").get_global_pos()
+	var se = boundaries.get_node("SE").get_global_pos()
+	var imagewidth = se.x - nw.x
+	var imageheight = se.y - nw.y
+	var gridy = ceil(float(imageheight) / Grid.GRID_SIZE.y)
+	var gridx = ceil(float(imagewidth) / Grid.GRID_SIZE.x)
+	var vertical_size = floor(imagewidth * HudMap.MAP_SCALE) - (gridwidth * (gridx - 1))
+	var horizontal_size = floor(imageheight * HudMap.MAP_SCALE) - (gridheight * (gridy - 1))
+	var vertical_area = vertical_size * gridheight
+	var horizontal_area = horizontal_size * gridwidth
+	var corner_area = horizontal_size * vertical_size
+	var normal_area = (gridx - 1) * gridwidth * (gridy - 1) * gridheight
+	var total = normal_area + vertical_area * (gridy - 1) + horizontal_area * (gridx - 1) + corner_area
+	var room_node = roomobj.instance()
+	room_node.get_node("title/value").set_text(filename)
+	room_node.get_node("grid/value").set_text(str(gridx) + "x" + str(gridy))
+	room_node.get_node("total/value").set_text(str(total))
+	room_node.get_node("size/value").set_text(str(normal_size))
+	room_node.get_node("horizontal/value").set_text(str(horizontal_area))
+	room_node.get_node("vertical/value").set_text(str(vertical_area))
+	room_node.get_node("corner/value").set_text(str(corner_area))
+	container.add_child(room_node)
+	return total
