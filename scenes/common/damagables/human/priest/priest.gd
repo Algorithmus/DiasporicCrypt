bfe91a98913d8fab79ba2e5e4d72fa169832a97c
Algorithmus <angelus.tenebrae@gmail.com>
2018-07-11 23:54:41 +0200

    Add more enemy graphics

---
+++ scenes/common/damagables/human/priest/priest.gd
@@ -0,0 +1,15 @@
+
+extends "res://scenes/common/damagables/BaseEnemy.gd"
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	hp = 1
+	is_consumable = true
+	consumable_size = Vector2(3, 1)
+	consumable_sprite_offset = Vector2(-16, 0)
+
+
+
