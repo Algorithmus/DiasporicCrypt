66e79c88a762aa9163b448ac5cd35f3b4bfc024f
Algorithmus <angelus.tenebrae@gmail.com>
2018-08-13 23:28:11 +0200

    Add more enemy graphics

--- scenes/common/damagables/plant/Plant.gd
+++ scenes/common/damagables/plant/Plant.gd
@@ -3,3 +3,10 @@ extends "res://scenes/common/damagables/statues/Statue.gd"
 
 func _ready():
 	ep = 100
+	follow_player = false
+
+func check_attack():
+	var offsetX = player.get_node("player").get_global_position().x - (get_global_position().x + projectile_offset.x)
+	#Shouldn't this be x? See https://github.com/godotengine/godot/issues/17405
+	if (offsetX != 0 && offsetX / abs(offsetX) != get_scale().y):
+		.check_attack()
