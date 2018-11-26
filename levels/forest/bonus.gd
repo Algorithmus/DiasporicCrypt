a55da4a54257a99a1a0005acb7b958afff213b6e
Algorithmus <angelus.tenebrae@gmail.com>
2018-11-26 23:34:21 +0100

    Use simpler illumination in forest levels.

--- levels/forest/bonus.gd
+++ levels/forest/bonus.gd
@@ -1,9 +1,10 @@
 
 extends Node2D
 
-var night = [Color(7/255.0, 8/255.0, 17/255.0), Color(7/255.0, 34/255.0, 58/255.0)]
-var sunrise = [Color(49/255.0, 69/255.0, 116/255.0), Color(1, 160/255.0, 69/255.0)]
+export var night = Color(47.8/255.0, 79.71/255.0, 147.42/255.0)
+export var sunrise = Color(1, 1, 1)
 var color
+var parallax_color
 var timer
 var limit
 
@@ -11,9 +12,10 @@ var limit
 func _ready():
 	ProjectSettings.set("blood_count", 0)
 	ProjectSettings.set("show_blood_counter", true)
-	color = get_node("ParallaxBackground/ParallaxLayer/sky").get_material()
-	color.set_shader_param("start", night[0])
-	color.set_shader_param("stop", night[1])
+	parallax_color = get_node("ParallaxBackground/CanvasModulate")
+	color = get_node("CanvasModulate")
+	parallax_color.set_color(night)
+	color.set_color(night)
 	var level = ProjectSettings.get("levels")[ProjectSettings.get("current_level")]
 	timer = level.time
 	limit = timer * 1.0
@@ -21,10 +23,9 @@ func _ready():
 
 func _physics_process(delta):
 	timer -= 1
-	var start = sunrise[0].linear_interpolate(night[0], ease(timer/limit, 0.4))
-	var stop = sunrise[1].linear_interpolate(night[1], ease(timer/limit, 0.4))
-	color.set_shader_param("start", start)
-	color.set_shader_param("stop", stop)
+	var color_value = sunrise.linear_interpolate(night, ease(timer/limit, 0.4))
+	color.set_color(color_value)
+	parallax_color.set_color(color_value)
 	if (timer <= 0):
 		ProjectSettings.set("sun", true)
 		set_physics_process(false)
